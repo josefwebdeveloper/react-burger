63433884dac0b4963f1b07968e3ca0169a685db7
Josef Shainskiy <josef2011@gmail.com>
2023-12-10 00:28:41 +0200

    header mock ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {AppHeader} from "./components/app-header/app-header";
 
 function App() {
   return (
     <div className="App">
-
+      <AppHeader/>
     </div>
   );
 }
