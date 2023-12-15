3b712fd313c28fbb10c8ffb691e58cbdf9fcb190
Josef Shainskiy <josef2011@gmail.com>
2023-12-15 16:07:22 +0200

    resolve comments

--- src/components/main/burger-ingredients/ingredients-group/ingredient/ingredient.tsx
+++ src/components/main/burger-ingredients/ingredients-group/ingredient/ingredient.tsx
@@ -3,14 +3,13 @@ import classNames from 'classnames';
 import { IngredientProps} from "../../../../../models/burger-data.model";
 import React from "react";
 import {Counter, CurrencyIcon} from "@ya.praktikum/react-developer-burger-ui-components";
-import {getImageNameFromUrl} from "../../../../../utils/utils";
 
 export const Ingredient: React.FC<IngredientProps> = ({ingredient}) => {
     return (
         <div  className={classNames(styles['ingredient-container'],)}>
             <Counter count={1} size="default" extraClass="m-1" />
             <div className={classNames(styles['ingredient-image'],'ml-4','mr-4','mb-1')}>
-                <img src={ingredient.image} alt={getImageNameFromUrl(ingredient.image)}/>
+                <img src={ingredient.image} alt={ingredient.image}/>
             </div>
             <div className={classNames(styles['ingredient-price'],'mb-1')}>
                 <span className='text text_type_digits-default mr-2'>{ingredient.price}</span>
